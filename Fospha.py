72a1382f78e4d0e385748231cc334e14a85bc385
TangoJamaicaFoxtrot <tylerjoelfox@gmail.com>
2026-02-06 12:42:50 +0000

    Update Fospha.py

--- Fospha.py
+++ Fospha.py
@@ -115,24 +115,18 @@ with tab1:
 
 
     Clear action: 
-    
     Reallocate spend from Paid Shopping into Performance Max and Paid Search while continuing to optimise Paid Social
     
     
     Budget & optimisation recommendations:
-    
     Increase investment in high-ROAS channels.
-    
     Scale Performance Max where possible.
-    
     Maintain and selectively grow Paid Search (Generic).
     
     Optimise mid-tier channels:
-    
     Paid Social requires creative, audience or bidding optimisation to improve efficiency.
     
     Reduce or reallocate low-ROAS spend:
-    
     Paid Shopping should be reviewed, optimised, or partially reallocated.
         """)
 
